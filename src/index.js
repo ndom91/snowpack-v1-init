f4e356001d575d6c385bee48e63283f2e0e026e7
ndo@ndo3 <yo@ni.co.de>
2019-08-22 20:49:57 +0200

    fix: rejiggered index.js and package.json paths

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-const commander = require('commander')
-const shell = require('shelljs')
-const { cyan, green, bold, underline} = require('kleur')
+const commander = require("commander");
+const shell = require("shelljs");
+const { cyan, green, bold, underline } = require("kleur");
 // const prompts = require('prompts')
-const envinfo = require('envinfo')
+const envinfo = require("envinfo");
 // let fs = require('fs')
-// const packageJson = require('./package.json')
+const packageJson = require("../../package.json");
 
-let appName
-let appDirectory = `${process.cwd()}/${appName}` // eslint-disable-line
+let appName;
+let appDirectory = `${process.cwd()}/${appName}`; // eslint-disable-line
 
 // let templates = require('../assets/templates/templates')
 
@@ -15,25 +15,25 @@ const createPikaApp = () => {
   return new Promise(resolve => {
     if (appName) {
       shell.exec(`create-react-app ${appName}`, () => {
-        console.log('Created react app')
-        resolve(true)
-      })
+        console.log("Created react app");
+        resolve(true);
+      });
     } else {
-      console.log(bold().red('\nNo app name was provided.'))
-      console.log(underline('\nUsage: '))
-      console.log('\ncreate-pika-app ', 'app-name\n'.cyan)
-      resolve(false)
+      console.log(bold().red("\nNo app name was provided."));
+      console.log(underline("\nUsage: "));
+      console.log("\ncreate-pika-app ", "app-name\n".cyan);
+      resolve(false);
     }
-  })
-}
+  });
+};
 
 const cdIntoNewApp = () => {
   return new Promise(resolve => {
     shell.exec(`cd ${appName}`, () => {
-      resolve()
-    })
-  })
-}
+      resolve();
+    });
+  });
+};
 
 // const copyTemplates = () => {
 //   return new Promise(resolve => {
@@ -60,105 +60,117 @@ const cdIntoNewApp = () => {
 
 const installDependencies = () => {
   return new Promise(resolve => {
-    console.log(cyan(
-      '\nInstalling preact, preact-compat, emotion, preact-emotion, and preact-router\n'
-    ))
+    console.log(
+      cyan(
+        "\nInstalling preact, preact-compat, emotion, preact-emotion, and preact-router\n"
+      )
+    );
     shell.exec(
       `npm install --save preact preact-compat preact-emotion preact-router emotion`,
       () => {
-        console.log(green('\nFinished installing dependencies\n'))
-        resolve()
-      },
-    )
-  })
-}
+        console.log(green("\nFinished installing dependencies\n"));
+        resolve();
+      }
+    );
+  });
+};
 
 const installDevDependencies = () => {
   return new Promise(resolve => {
-    console.log(cyan(
-      '\nInstalling @pika/web, typescript, eslint, serve, babel, and all their required plugins/presets\n'
-    ))
+    console.log(
+      cyan(
+        "\nInstalling @pika/web, typescript, eslint, serve, babel, and all their required plugins/presets\n"
+      )
+    );
     shell.exec(
       `npm install -D @babel/cli @babel/core @pika/web @typescript-eslint/eslint-plugin @typescript-eslint/parser babel-plugin-import-pika-web copyfiles prettier eslint eslint-config-airbnb-typescript eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react serve typescript`,
       () => {
-        console.log(green('\nFinished installing dev dependencies\n'))
-        resolve()
-      },
-    )
-  })
-}
-
+        console.log(green("\nFinished installing dev dependencies\n"));
+        resolve();
+      }
+    );
+  });
+};
 
 const run = async () => {
-  const program = new commander.Command('create-pika-app')
-    .version('0.0.2')
-    .arguments('<project-directory>')
-    .usage(`${green('<project-directory>')} [options]`)
+  const program = new commander.Command(packageJson.name)
+    .version(packageJson.version)
+    .arguments("<project-directory>")
+    .usage(`${green("<project-directory>")} [options]`)
     .action(name => {
-      appName = name
+      appName = name;
     })
-    .option('--verbose', 'print additional logs')
-    .option('--info', 'print environment debug info')
+    .option("--verbose", "print additional logs")
+    .option("--info", "print environment debug info")
     .allowUnknownOption()
-    .on('--help', () => {
-        console.log(`    Only ${green('<project-directory>')} is required.`);
-        console.log();
-        console.log( `    If you have any problems, do not hesitate to file an issue:`);
-        console.log( `      ${cyan( 'https://github.com/ndom91/create-pika-app/issues/new')}`); 
-        console.log()
+    .on("--help", () => {
+      console.log(`    Only ${green("<project-directory>")} is required.`);
+      console.log();
+      console.log(
+        `    If you have any problems, do not hesitate to file an issue:`
+      );
+      console.log(
+        `      ${cyan("https://github.com/ndom91/create-pika-app/issues/new")}`
+      );
+      console.log();
     })
-    .parse(process.argv) // eslint-disable-line
+    .parse(process.argv); // eslint-disable-line
 
   if (program.info) {
-    console.log(bold('\nEnvironment Info:'))
+    console.log(bold("\nEnvironment Info:"));
     return envinfo
       .run(
         {
-          System: ['OS', 'CPU'],
-          Binaries: ['Node', 'npm' ],
+          System: ["OS", "CPU"],
+          Binaries: ["Node", "npm"],
           Browsers: [
-            'Chrome',
-            'Edge',
-            'Internet Explorer',
-            'Firefox',
-            'Safari',
+            "Chrome",
+            "Edge",
+            "Internet Explorer",
+            "Firefox",
+            "Safari"
           ],
-          npmPackages: ['preact', 'preact-compat', '@pika/web', 'preact-emotion'],
-          npmGlobalPackages: ['create-pika-app'],
+          npmPackages: [
+            "preact",
+            "preact-compat",
+            "@pika/web",
+            "preact-emotion"
+          ],
+          npmGlobalPackages: ["create-pika-app"]
         },
         {
           duplicates: true,
-          showNotFound: true,
-        },
+          showNotFound: true
+        }
       )
-      .then(console.log)
+      .then(console.log);
   }
 
-  if (typeof projectName === 'undefined') {
-    console.error('Please specify the project directory:')
-    console.log(`  ${cyan(program.name())} ${green('<project-directory>')}`)
-    console.log()
-    console.log('For example:')
-    console.log(`  ${cyan(program.name())} ${green('my-pika-app')}`)
-    console.log()
-    console.log(`Run ${cyan(`${program.name()} --help`)} to see all options.`)
-    process.exit(1) // eslint-disable-line
+  if (typeof projectName === "undefined") {
+    console.error("Please specify the project directory:");
+    console.log(`  ${cyan(program.name())} ${green("<project-directory>")}`);
+    console.log();
+    console.log("For example:");
+    console.log(`  ${cyan(program.name())} ${green("my-pika-app")}`);
+    console.log();
+    console.log(`Run ${cyan(`${program.name()} --help`)} to see all options.`);
+    process.exit(1); // eslint-disable-line
   }
 
-  let success = await createPikaApp()
+  let success = await createPikaApp();
   if (!success) {
     console.log(
       bold().red(
-        'Something went wrong while trying to create a new Preact app using create-pika-app',
-      ),
-    )
-    return false
+        "Something went wrong while trying to create a new Preact app using create-pika-app"
+      )
+    );
+    return false;
   }
-  await cdIntoNewApp()
+  await cdIntoNewApp();
   // await copyTemplates()
-  await installDependencies()
-  await installDevDependencies()
-  console.log(bold().green('All done 🎉'))
-}
+  await installDependencies();
+  await installDevDependencies();
+  console.log(bold().green("All done 🎉"));
+};
 
-run()
+run();
