223c399ec1f8cdc392882f30bee2c82d78ad277d
Dustin Masters <ceo@dustinsoftware.com>
2020-01-20 22:05:06 -0800

    Changes from `npm run prettier`

--- assets/app-lit/template/components/todo-header.js
+++ assets/app-lit/template/components/todo-header.js
@@ -1,3 +1,6 @@
-import { html } from '../web_modules/lit-html.js';
+import { html } from '../web_modules/lit-html.js'
 
-export const header = name => html`<header><h2>${name}</h2></header>`;
+export const header = name =>
+  html`
+    <header><h2>${name}</h2></header>
+  `
--- assets/app-lit/template/js/index.js
+++ assets/app-lit/template/js/index.js
@@ -1,3 +1,3 @@
-import { LitSnowpack } from '../components/lit-snowpack.js';
+import { LitSnowpack } from '../components/lit-snowpack.js'
 
-window.customElements.define('lit-snowpack', LitSnowpack);
+window.customElements.define('lit-snowpack', LitSnowpack)
